e8ad53f38fdb61b6abbc5162a8d119f4aa3d5b0f
Daniel Afonso <danieljcafonso@gmail.com>
2023-05-24 16:24:10 +0100

    refactor: cleanup exercise 6

--- src/utils/test-utils.js
+++ src/utils/test-utils.js
@@ -5,17 +5,15 @@ import { SnackbarProvider } from "notistack";
 
 export const dummyUserData = { username: "daniel", email: "daniel@admin.com" };
 
-export const dummyCarData = {
-  data: {
-    thisisacarid: {
-      brand: "Audi",
-      model: "Guinea",
-      segment: "Van",
-      price: 12000,
-      fuel: "Diesel",
-      photo:
-        "https://as2.ftcdn.net/v2/jpg/00/16/14/89/1000_F_16148967_YvRk9vkq8eyVda5pDAeTRCvciG87ucqJ.jpg",
-    },
+export const dummyCarList = {
+  thisisacarid: {
+    brand: "Audi",
+    model: "Guinea",
+    segment: "Van",
+    price: 12000,
+    fuel: "Diesel",
+    photo:
+      "https://as2.ftcdn.net/v2/jpg/00/16/14/89/1000_F_16148967_YvRk9vkq8eyVda5pDAeTRCvciG87ucqJ.jpg",
   },
 };
 

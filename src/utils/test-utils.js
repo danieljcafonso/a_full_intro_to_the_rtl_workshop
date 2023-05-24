f47c882a1017699f26d8ffec3f2081372de61a35
Daniel Afonso <danieljcafonso@gmail.com>
2023-05-24 17:39:06 +0100

    feat: add initial README and refactor

--- src/utils/test-utils.js
+++ src/utils/test-utils.js
@@ -3,6 +3,32 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "../context/AuthContext";
 import { SnackbarProvider } from "notistack";
 
+export const dummyUserData = { username: "daniel", email: "daniel@admin.com" };
+
+export const dummyCarData = {
+  data: {
+    thisisacarid: {
+      brand: "Audi",
+      model: "Guinea",
+      segment: "Van",
+      price: 12000,
+      fuel: "Diesel",
+      photo:
+        "https://as2.ftcdn.net/v2/jpg/00/16/14/89/1000_F_16148967_YvRk9vkq8eyVda5pDAeTRCvciG87ucqJ.jpg",
+    },
+  },
+};
+
+export const dummyCarCreateData = {
+  brand: "Audi",
+  model: "Guinea",
+  segment: "Van",
+  price: "12000",
+  fuel: "Diesel",
+  photo:
+    "https://as2.ftcdn.net/v2/jpg/00/16/14/89/1000_F_16148967_YvRk9vkq8eyVda5pDAeTRCvciG87ucqJ.jpg",
+};
+
 const customRender = (ui, { ...options } = {}) => {
   const queryClient = new QueryClient({
     logger: {

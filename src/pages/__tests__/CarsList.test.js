2254bd8920f36bb0b3286b9cc826a401641ed374
Daniel Afonso <danieljcafonso@gmail.com>
2023-05-24 15:57:59 +0100

    refactor: cleanup exercise 2

--- src/pages/__tests__/CarsList.test.js
+++ src/pages/__tests__/CarsList.test.js
@@ -1,36 +1,12 @@
 import CarsList from "../CarsList";
-import { render, screen, within } from "../../utils/test-utils";
-import userEvent from "@testing-library/user-event";
-import * as useLocalStorage from "../../hooks/useLocalStorage";
+import { render, screen, dummyCarData } from "../../utils/test-utils";
 import { axiosInstance } from "../../api/carsAPI";
 
 const getSpy = jest.spyOn(axiosInstance, "get");
 
-const dummyCarData = {
-  data: {
-    thisisacarid: {
-      brand: "Audi",
-      model: "Guinea",
-      segment: "Van",
-      price: 12000,
-      fuel: "Diesel",
-      photo:
-        "https://as2.ftcdn.net/v2/jpg/00/16/14/89/1000_F_16148967_YvRk9vkq8eyVda5pDAeTRCvciG87ucqJ.jpg",
-    },
-  },
-};
-
-const useLocalStorageOriginalImplementation = useLocalStorage.default;
-const dummyUserData = { username: "daniel", email: "daniel@admin.com" };
-
 describe("CarsList tests", () => {
   beforeEach(() => {
-    useLocalStorage.default = jest.fn(() => [dummyUserData, jest.fn()]);
     getSpy.mockResolvedValue(dummyCarData);
-  });
-
-  afterAll(() => {
-    useLocalStorage.default = useLocalStorageOriginalImplementation;
   });
 
   it("should show loading spinner", async () => {

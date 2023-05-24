891e009164abcb4166881db0eb63492ffb580d7c
Daniel Afonso <danieljcafonso@gmail.com>
2023-05-24 16:11:22 +0100

    refactor: cleanup exercise 5

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "../../utils/test-utils";
+import { render, screen, waitFor, dummyUserData } from "../../utils/test-utils";
 import Header from "../Header";
 import * as useLocalStorage from "../../hooks/useLocalStorage";
 import { useNavigate, useLocation } from "react-router-dom";
@@ -7,17 +7,12 @@ import userEvent from "@testing-library/user-event";
 const navigateMockFn = jest.fn();
 
 const setLocalStorage = jest.fn();
-const useLocalStorageOriginalImplementation = useLocalStorage.default;
-const dummyUserData = { username: "daniel", email: "daniel@admin.com" };
 
 describe("Header tests", () => {
   beforeEach(() => {
     useLocalStorage.default = jest.fn(() => [null, setLocalStorage]);
     useNavigate.mockImplementation(() => navigateMockFn);
     useLocation.mockImplementation(() => ({ pathname: "/" }));
-  });
-  afterAll(() => {
-    useLocalStorage.default = useLocalStorageOriginalImplementation;
   });
 
   it("should render", () => {
